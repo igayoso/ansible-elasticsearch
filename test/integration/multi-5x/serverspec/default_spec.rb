bc3909abad418e7d54e9f0fc075fd54ca9f7b0ea
igayoso <igayoso@gmail.com>
2017-06-15 19:05:27 +0200

    Upgraded to 5.4.1

--- test/integration/multi-5x/serverspec/default_spec.rb
+++ test/integration/multi-5x/serverspec/default_spec.rb
@@ -2,7 +2,7 @@ require 'multi_spec'
 
 
 describe 'Multi Tests v 5.x' do
-  include_examples 'multi::init', "5.3.1", ["ingest-geoip"]
+  include_examples 'multi::init', "5.4.1", ["ingest-geoip"]
 end
 
 
--- test/integration/xpack-5x/serverspec/default_spec.rb
+++ test/integration/xpack-5x/serverspec/default_spec.rb
@@ -1,5 +1,5 @@
 require 'xpack_spec'
 
 describe 'Xpack Tests v 5.x' do
-  include_examples 'xpack::init', "5.3.1", ["ingest-attachment"]
+  include_examples 'xpack::init', "5.4.1", ["ingest-attachment"]
 end
